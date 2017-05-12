c2b7e0b60040e90b628fcb747247e4f2fed794b4
govnokodishe <abekirev@gmail.com>
2017-05-12 23:18:12 +0300

    replace resource usage in task12 of 2016 with IOUtils

--- src/main/kotlin/year2016/task12/Main.kt
+++ src/main/kotlin/year2016/task12/Main.kt
@@ -1,29 +1,25 @@
 package year2016.task12
 
+import common.IOUtils
 import year2016.task12.computer.Computer
-import year2016.task12.computer.program.instruction.factory.InstructionFactory
-import year2016.task12.computer.program.ArrayListBasedProgram
 import year2016.task12.computer.processor.Processor
 import year2016.task12.computer.processor.RegisterNames
 import year2016.task12.computer.processor.immutable.ImmutableProcessor
 import year2016.task12.computer.processor.mutable.MutableProcessor
+import year2016.task12.computer.program.ArrayListBasedProgram
+import year2016.task12.computer.program.instruction.factory.InstructionFactory
 import year2016.task12.runner.UntilHaltedComputerRunner
-import java.io.InputStreamReader
-import java.io.Reader
 
 object Main {
     val program = ArrayListBasedProgram(
-            javaClass.getResourceAsStream("/year2016/task12/input.txt").use { inputStream ->
-                InputStreamReader(inputStream).use { reader: Reader ->
-                    reader.useLines { lines ->
-                        lines.map(InstructionFactory::fromString).toList()
-                    }
-                }
+            IOUtils.useLinesFromResource("/year2016/task12/input.txt") { lines ->
+                lines.map(InstructionFactory::fromString).toList()
             }
     )
 
     fun run(processorFactory: () -> Processor) {
-        val computer = UntilHaltedComputerRunner(Computer(processorFactory().writeRegister(RegisterNames.C, 1), program)).run()
+        val computer = UntilHaltedComputerRunner(Computer(processorFactory().writeRegister(RegisterNames.C, 1),
+                                                          program)).run()
         println("a: ${computer.peek(RegisterNames.A)}")
     }
 }
